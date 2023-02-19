8343c0ebb1bd5af3fb0bc7cd8e2dcac2223cf1f5
Gabriel Okamoto <gabrielokamoto@hotmail.com>
2023-02-19 01:43:24 -0300

    temp

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-function main(){}
+import { plot } from "nodeplotlib";
+
+import Elevator from "./Elevator";
+
+function main() {
+  const e = new Elevator(2, 0.5);
+  const desiredPos = 20; // meters
+  const dt = 1e-3; // 100 milliseconds
+  const allowedError = 1e-3; // 1 cm
+  const positions: number[] = [];
+  const stopPositions: number[] = [];
+  while (Math.abs(e.position - desiredPos) > allowedError) {
+    e.tick(desiredPos, dt);
+    positions.push(e.position);
+    stopPositions.push(e.stopPosition());
+  }
+
+  const x = positions.map((_, i) => i * dt);
+  const plotData = [
+    {
+      x,
+      y: positions,
+      type: "scatter" as const,
+    },
+    {
+      x,
+      y: stopPositions,
+      type: "scatter" as const,
+    },
+  ];
+  plot(plotData);
+}
+
+main();
